ac56c8e22bbab329d4ff18f0e0069bde0d187008
bullego <lego.bul@gmail.com>
2020-07-02 16:43:28 +0300

    Updates

---
+++ precache-manifest.486fba60f8ac0fe355e85488534fb437.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b977eca600517bb572bb9ca3b9e21368",
+    "url": "/web-academy_OAuth/index.html"
+  },
+  {
+    "revision": "08ae6023c48f2930b29d",
+    "url": "/web-academy_OAuth/static/js/2.9f9efa78.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/web-academy_OAuth/static/js/2.9f9efa78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7d4c6c66c4530a6edef",
+    "url": "/web-academy_OAuth/static/js/main.246b2df7.chunk.js"
+  },
+  {
+    "revision": "ed1169a892f929b0fd08",
+    "url": "/web-academy_OAuth/static/js/runtime-main.b368c138.js"
+  }
+]);
